55afcfbf7af08264c7996c78a102d90e9ff670e3
Mrigendra Soni <mrigendrasoni26@gmail.com>
2023-07-10 17:30:18 +0530

    Update FundMe.sol

--- PatrickCollinsCourse/Lesson 4/FundMe.sol
+++ PatrickCollinsCourse/Lesson 4/FundMe.sol
@@ -2,33 +2,19 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
 
+    using PriceConverter for uint256;
+
     uint256 public minimumUsd = 5e18;
-    address[] public .funders;
+    address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
     function fund() public payable {
-        require(getConversionRate(msg.value) >= minimumUsd, "not enough eth");
+        require(msg.value.getConversionRate() >= minimumUsd, "not enough eth");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
-    }
-
-    //function withdraw() public {
-    //}
-    function getPrice() public view returns(uint256) {
-        (,int answer,,,) = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).latestRoundData();
-        return uint256(answer*1e10);
-    }
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
-
-    function getVersion() public view returns (uint256) {
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 }
